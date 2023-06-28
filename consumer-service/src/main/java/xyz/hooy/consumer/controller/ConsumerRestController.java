058e4280869c66d384cca01abf7ad90c0779771c
hooyantsing <huyanqing@e-byte.com>
2023-06-28 15:14:18 +0800

    [Feat]: 服务消费者 consumer 集群

--- consumer-service/src/main/java/xyz/hooy/consumer/controller/ConsumerRestController.java
+++ consumer-service/src/main/java/xyz/hooy/consumer/controller/ConsumerRestController.java
@@ -1,5 +1,6 @@
 package xyz.hooy.consumer.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,13 @@ public class ConsumerRestController {
     @GetMapping("/hello-world")
     public String testConsumerRestController(){
         return "hello world";
+    }
+
+    @Value("${spring.cloud.nacos.config.group}")
+    private String configGroup;
+
+    @GetMapping("/config-group")
+    public String testConfigGroup(){
+        return configGroup;
     }
 }

fcc89b4a3b5b38e7c7b74709341f4d34d60522f9
hooyantsing <huyanqing@e-byte.com>
2023-06-29 14:22:07 +0800

    [Feat]: zookeeper 3.8.1 集群

--- consumer-service/src/main/java/xyz/hooy/consumer/ConsumerServiceApplication.java
+++ consumer-service/src/main/java/xyz/hooy/consumer/ConsumerServiceApplication.java
@@ -2,9 +2,7 @@ package xyz.hooy.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient // 开启注册与发现服务
 @SpringBootApplication
 public class ConsumerServiceApplication {
 
